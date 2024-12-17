9f8a1d0d92fdee4db9f95f203185ebfab93108d7
Kauan Felipe <kauan_falima@hotmail.com>
2024-12-17 00:57:07 -0300

    add base vite config

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "CodeQuest",
   server: {
     proxy: {
       '/api': 'http://localhost:3000', // Redireciona chamadas para o backend
